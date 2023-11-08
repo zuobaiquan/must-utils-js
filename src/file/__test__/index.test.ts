b6f33bbad09bddc903aae0c9115d88538f0c91c6
zuobaiquan <zuobaiquan@qq.com>
2023-11-08 16:31:33 +0800

    feat: 增加测试

--- src/file/__test__/index.test.ts
+++ src/file/__test__/index.test.ts
@@ -2,9 +2,9 @@ import { getFileType,getLastName } from "..";
 
 test("test getFileType", () => {
   expect(getFileType("index.html")).toEqual("html");
-  expect(getFileType("https://static.htd.cn/yfzx/2023829/t1693302659947blobid0.png")).toEqual("png");
+  expect(getFileType("https://static.xxx.cn/upload/2023829/t1693302659947blobid0.png")).toEqual("png");
 });
 test("test getLastName", () => {
   expect(getLastName("index.html")).toEqual("index.html");
-  expect(getLastName("https://static.htd.cn/yfzx/2023829/t1693302659947blobid0.png")).toEqual("t1693302659947blobid0.png");
+  expect(getLastName("https://static.xxx.cn/upload/2023829/t1693302659947blobid0.png")).toEqual("t1693302659947blobid0.png");
 });
